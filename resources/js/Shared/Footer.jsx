cc17b6770a48c3603a326ea6896cbb134a434e06
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-08-21 23:03:15 +0500

    footer code refactored

--- resources/js/Shared/Footer.jsx
+++ resources/js/Shared/Footer.jsx
@@ -1,6 +1,7 @@
 import { Link } from "@inertiajs/react"
 import JewelleryLogo from './../../images/JewelleryLogo.png';
 import { FaFacebookF, FaInstagram, FaPinterestP, FaXTwitter, FaYoutube } from "react-icons/fa6";
+import { FooterNavContainer } from "./FooterNavContainer";
 export let Footer = () => {
 
 
@@ -8,33 +9,32 @@ export let Footer = () => {
         {
             name: "Company",
             links: [
-                { name: 'About Us', link: '/about' },
-                { name: 'Blog', link: '/blog' },
-                { name: 'Contact Us', link: '/contact' },
-                { name: 'Career', link: '/career' },
+                { name: 'About Us', path: '/about' },
+                { name: 'Blog', path: '/blog' },
+                { name: 'Contact Us', path: '/contact' },
+                { name: 'Career', path: '/career' },
             ]
         },
         {
             name: "Customer Services",
             links: [
-                { name: 'My Account', link: '/account' },
-                { name: 'Track Your Order', link: '/track' },
-                { name: 'Return', link: '/return' },
-                { name: 'FAQ', link: '/faq' },
+                { name: 'My Account', path: '/account' },
+                { name: 'Track Your Order', path: '/track' },
+                { name: 'Return', path: '/return' },
+                { name: 'FAQ', path: '/faq' },
             ]
         },
         {
             name: "Our Information",
             links: [
-                { name: 'Privacy', link: '/privacy' },
-                { name: 'User Terms & Conditions', link: '/terms' },
-                { name: 'Return Policy', link: '/policy/return' },
+                { name: 'Privacy', path: '/privacy' },
+                { name: 'User Terms & Conditions', path: '/terms' },
+                { name: 'Return Policy', path: '/policy/return' },
             ]
         },
     ];
 
-
-    const contactInfo = ['+0123-456-789', 'example@gmail.com', 'Warehouse #5, Industrial Zone, I-9/3, Islamabad, Pakistan'];
+    const contactInfo = [{ name: '+0123-456-789', path: '#' }, { name: 'example@gmail.com', path: '#' }, { name: 'Warehouse #5, Industrial Zone, I-9/3, Islamabad, Pakistan', path: '#' }];
 
     return <footer className="px-20 py-15 w-full">
         <div className="w-full">
@@ -53,26 +53,11 @@ export let Footer = () => {
                         <div><Link href={'#'} className="inline-block p-2 bg-mustard shadow"><FaPinterestP className="text-zinc text-lg" /></Link></div>
                         <div><Link href={'#'} className="inline-block p-2 bg-mustard shadow"><FaInstagram className="text-zinc text-lg" /></Link></div>
                         <div><Link href={'#'} className="inline-block p-2 bg-mustard shadow"><FaYoutube className="text-zinc text-lg" /></Link></div>
-
                     </div>
                 </div>
                 <div className="w-[78%] flex flex-row justify-between items-start gap-x-2">
-                    {footerLink.map((section, idx) => <div className="w-[25%] flex flex-col justify-between items-start gap-y-5" key={`footerLinkContainer${idx + 1}`}>
-
-                        <h3 className="font-poppins font-medium text-lg">{section.name}</h3>
-                        <ul className="w-full flex flex-col gap-y-3 items-start justify-between">
-                            {section.links.map((link, index) => <li key={`footerSectionLink${index + 1}`}><Link href={link.link} className="inline-block text-sm text-light-gray font-medium hover:underline">{link.name}</Link></li>)}
-
-                        </ul>
-                    </div>
-
-                    )}
-                    <div className=" flex flex-col justify-between items-start gap-y-5 w-[25%]">
-                        <h3 className="font-poppins font-medium text-lg">Contact Info</h3>
-                        <ul className="w-full flex flex-col gap-y-3 items-start justify-between">
-                            {contactInfo.map((name, index) => <li key={`footerSectionLink${index + 1}`}><Link href={'#'} className="inline-block text-sm text-light-gray font-medium hover:underline">{name}</Link></li>)}
-                        </ul>
-                    </div>
+                    {footerLink.map((section, idx) => <FooterNavContainer key={`footerSection${idx + 1}`} heading={section.name} links={section.links} />)}
+                    <FooterNavContainer heading={'Contact Info'} links={contactInfo} />
                 </div>
             </div>
             <div className="flex border-t-[1px] border-light-gray py-8 w-full items-center justify-center">
