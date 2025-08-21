7951463694ef37d21bbf5d6cd66c15dc87cabb21
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-08-21 17:38:29 +0500

    Header Completed

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,9 +1,11 @@
 import { SaleHeading } from "../Components/SaleHeading";
+import { Header } from "../Shared/Header";
 
 export default function Home() {
     return (
         <section className="w-full">
             <SaleHeading />
+            <Header />
         </section>
     );
 }
---
+++ resources/js/Shared/NavLink.jsx
@@ -0,0 +1,7 @@
+import { Link, usePage } from "@inertiajs/react";
+
+export let NavLink = ({ link, name }) => {
+    let { url } = usePage();
+    console.log(url)
+    return <li><Link className={`text-zinc font-poppins font-medium hover:font-semibold ${url === link ? 'underline font-semibold' : ''}`} href={link}>{name}</Link></li>;
+}
