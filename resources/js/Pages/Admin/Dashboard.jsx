0a5e9b4bd352ac9eed4f0608b77d42ad40f9ecb9
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-08-22 18:39:45 +0500

    Admin Header Completed

--- resources/js/Pages/Admin/Dashboard.jsx
+++ resources/js/Pages/Admin/Dashboard.jsx
@@ -1,17 +1,16 @@
 
 import { usePage } from "@inertiajs/react";
 import { Sidebar } from "../../Shared/Admin/Sidebar";
+import { Header } from "../../Shared/Admin/Header";
 
 export default function Dashboard() {
-    const { auth } = usePage().props || {};
-    const adminName = auth?.firstName || "Admin";
-
-
     return (
         <section className="w-full h-screen min-h-170">
             <div className="w-full flex flex-row h-full">
                 <Sidebar />
-                <div className="bg-powder-gray min-h-full flex-1"></div>
+                <div className="bg-powder-gray min-h-full flex-1">
+                    <Header />
+                </div>
             </div>
         </section>
     );
---
+++ resources/js/Shared/Admin/Header.jsx
@@ -0,0 +1,54 @@
+import { format } from "date-fns";
+import { LuCalendarDays } from "react-icons/lu";
+import { FaBell } from "react-icons/fa";
+import { usePage } from "@inertiajs/react";
+import { FaCaretDown } from "react-icons/fa6";
+
+let FormattedDate = () => {
+    const date = new Date();
+    const formatted = format(date, "EEEE, do MMMM");
+    return <p className="flex items-center justify-center gap-x-1"><span><LuCalendarDays className="text-light-gray text-lg" /></span><span className="text-light-gray text-sm text-medium font-poppins">{formatted}</span></p>
+}
+let NotificationIcon = ({ count = '0' }) => {
+    return <div>
+        <button type="button" className="p-1 relative cursor-pointer">
+            <span><FaBell className="text-2xl text-zinc" /></span>
+            <span className="absolute flex items-center justify-center p-1 bg-mustard text-white font-poppins rounded-full h-4 w-4 text-xs -top-0.5 -right-0.5">{count}</span>
+        </button>
+    </div>
+}
+
+let AdminProfileSection = () => {
+
+    const { auth } = usePage().props || {};
+    let adminFirstName = auth?.firstName || "Super";
+    let adminLastName = auth?.lastName || "Admin";
+    let adminName = `${adminFirstName} ${adminLastName}`
+    return (
+        <div className="flex flex-row gap-x-1.5">
+            <div className="flex flex-col items-end justify-center">
+                <h6 className="font-poppins font-semibold text-semi-black text-base">{adminName}</h6>
+                <p className="font-poppins text-light-gray -mt-0.5 text-sm">Shop Owner</p>
+            </div>
+            <div className="flex items-center justify-center">
+                <button className="w-18 flex items-center justify-end gap-x-1 cursor-pointer">
+                    <div className="rounded-full h-12 w-12 overflow-hidden flex items-center justify-center shadow"><img src="https://www.shareicon.net/data/2016/07/05/791214_man_512x512.png" alt="Profile Picture" />
+                    </div>
+                    <span><FaCaretDown className="text-light-gray text-base" /></span>
+                </button>
+            </div>
+        </div>
+    );
+}
+export let Header = () => {
+    return <header className="w-full bg-white shadow py-3 px-5">
+        <div className="w-full flex flex-row items-center justify-end">
+            <div className=""></div>
+            <div className="gap-x-7 flex flex-row items-center justify-between py-0">
+                <FormattedDate />
+                <NotificationIcon count="0" />
+                <AdminProfileSection />
+            </div>
+        </div>
+    </header>
+}
