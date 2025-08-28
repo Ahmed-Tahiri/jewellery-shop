9d1a1c6b2667dad8dfad6e54b650b358f01ed116
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-08-28 10:21:52 +0500

    Password Page is responsive

--- resources/js/Pages/MyAccount/Password.jsx
+++ resources/js/Pages/MyAccount/Password.jsx
@@ -26,13 +26,13 @@ export default function Password() {
         );
     }
     return (
-        <div className="w-7/9 flex flex-col items-start gap-y-7">
+        <div className="lg:w-6/8 md:w-5/7 w-10/12 flex flex-col items-start gap-y-7">
             <form onSubmit={formSubmitHandler} className="w-full flex flex-col items-start gap-y-4">
                 <div className="flex flex-col items-start w-full gap-y-3">
                     <PasswordField label={'Current Password'} data={data.current_password} inputChangeHandler={inputChangeHandler} name="current_password" id="currentPassword" placeholder="Enter Current Password" error={errors.current_password} />
 
                     <div className="w-full flex justify-end">
-                        <Link href={'/myaccount/password/forgot'} className="font-poppins text-sm font-semibold text-zinc underline hover:text-zinc-dark active:text-mustard">Forgot Password?</Link>
+                        <Link href={'/myaccount/password/forgot'} className="font-poppins -mt-1.5 sm:mt-0 text-xs md:text-sm font-semibold text-zinc underline hover:text-zinc-dark active:text-mustard">Forgot Password?</Link>
                     </div>
                     <div className={`${isSuccessful ? 'block' : 'hidden'}`}>
                         <span className="text-green-700 font-poppins text-sm">Password Updated Successfully</span>
@@ -41,7 +41,7 @@ export default function Password() {
                 <PasswordField label={'New Password'} data={data.new_password} inputChangeHandler={inputChangeHandler} name="new_password" id="newPassword" placeholder="Enter New Password" error={errors.new_password} />
                 <PasswordField label={'Confirm New Password'} data={data.new_password_confirmation} inputChangeHandler={inputChangeHandler} name="new_password_confirmation" id="newPasswordConfirmation" placeholder="Re-enter New Password" error={errors.new_password_confirmation} />
                 <div>
-                    <button type="submit" disabled={processing} className={`font-poppins w-50 shadow-xs bg-zinc text-white hover:bg-zinc-dark cursor-pointer py-2  px-3 text-center ${processing ? 'opacity-50' : 'opacity-100'}`}>
+                    <button type="submit" disabled={processing} className={`font-poppins md:text-base text-sm w-50 shadow-xs bg-zinc text-white hover:bg-zinc-dark cursor-pointer py-2  px-3 text-center ${processing ? 'opacity-50' : 'opacity-100'}`}>
                         {processing ? 'Updating Password...' : 'Update Password'}
                     </button>
                 </div>
