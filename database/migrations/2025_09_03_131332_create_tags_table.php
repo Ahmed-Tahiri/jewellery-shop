f2489135a3c8e84c7b9d9ba26857177ab615632d
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-03 19:09:45 +0500

    Timestamp added into product_tag

--- database/migrations/2025_09_03_131332_create_tags_table.php
+++ database/migrations/2025_09_03_131332_create_tags_table.php
@@ -20,6 +20,7 @@ return new class extends Migration
             $table->primary(['product_id', 'tag_id']);
             $table->foreignId('product_id')->constrained('products')->cascadeOnDelete();
             $table->foreignId('tag_id')->constrained('tags')->cascadeOnDelete();
+            $table->timestamps();
         });
     }
 
