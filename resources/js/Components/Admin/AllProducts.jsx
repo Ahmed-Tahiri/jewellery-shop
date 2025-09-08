0b33ad1a96c1490ba403680bb6a098ac130fd132
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-08 19:45:00 +0500

    heading also changed

--- resources/js/Components/Admin/AllProducts.jsx
+++ resources/js/Components/Admin/AllProducts.jsx
@@ -7,7 +7,7 @@ export let AllProducts = ({ products }) => {
         <div className="flex w-full bg-white shadow rounded flex-col gap-y-5 p-5">
 
             <div className={`w-full flex flex-row gap-x-5  items-center justify-between border-b-[1px] border-gray-300 pb-4`}>
-                <h4 className={`font-poppins text-lg font-medium text-semi-black `}>Parent Categories</h4>
+                <h4 className={`font-poppins text-lg font-medium text-semi-black `}>All Products</h4>
                 <SectionLink path={'admin.products.create'} title={'Add Product'} />
             </div>
             <div className="w-full flex flex-row gap-10 flex-wrap items-start justify-start">
