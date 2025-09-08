5f7ea2625d59b6020388a9a29744146aa3bd0628
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-08 19:44:23 +0500

    link bug fixed

--- resources/js/Components/Admin/AllProducts.jsx
+++ resources/js/Components/Admin/AllProducts.jsx
@@ -8,7 +8,7 @@ export let AllProducts = ({ products }) => {
 
             <div className={`w-full flex flex-row gap-x-5  items-center justify-between border-b-[1px] border-gray-300 pb-4`}>
                 <h4 className={`font-poppins text-lg font-medium text-semi-black `}>Parent Categories</h4>
-                <SectionLink path={'admin.categories.create'} title={'Add Category'} />
+                <SectionLink path={'admin.products.create'} title={'Add Product'} />
             </div>
             <div className="w-full flex flex-row gap-10 flex-wrap items-start justify-start">
 
