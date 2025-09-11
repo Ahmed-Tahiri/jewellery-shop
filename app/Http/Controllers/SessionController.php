05fbe1b41d1ae6f03891f2d60098bf26b95f639c
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-11 21:08:54 +0500

    comment removed

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use App\Models\LoginAttempt;
-use App\Models\Session;
-use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
