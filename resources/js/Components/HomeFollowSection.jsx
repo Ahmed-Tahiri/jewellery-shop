b9f29afa075c44ded0604677e1b68a7f7890969e
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-14 23:49:06 +0500

    Follow Us Page Completed

--- resources/js/Components/HomeFollowSection.jsx
+++ resources/js/Components/HomeFollowSection.jsx
@@ -1,5 +1,14 @@
 import { SectionMainHeading } from "../Shared/SectionMainHeading";
 import { SectionSubHeading } from "../Shared/SectionSubHeading";
+import FollowUsMainImg from './../../images/FollowUsMainImg.jpg';
+import FollowImg1 from './../../images/followImg1.jpg';
+import FollowImg2 from './../../images/followImg2.jpg';
+import FollowImg3 from './../../images/followImg3.jpg';
+import FollowImg4 from './../../images/followImg4.jpg';
+import FollowImg5 from './../../images/followImg5.jpg';
+import FollowImg6 from './../../images/followImg6.jpg';
+import FollowImg7 from './../../images/followImg7.jpg';
+import FollowImg8 from './../../images/followImg8.jpg';
 
 
 export let HomeFollowSection = () => {
@@ -10,8 +19,20 @@ export let HomeFollowSection = () => {
                 <SectionSubHeading heading={'Follow Us'} />
                 <SectionMainHeading heading={'Follow Us On Instagram'} />
             </div>
-            <div className='w-full gap-x-5 flex items-start justify-between'>
-
+            <div className='w-full gap-5 flex flex-row items-center justify-between'>
+                <div className="w-1/3 h-100 flex flex-row flex-wrap  gap-5">
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg1} alt="Follow Sub Image" /></div>
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg2} alt="Follow Sub Image" /></div>
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg3} alt="Follow Sub Image" /></div>
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg4} alt="Follow Sub Image" /></div>
+                </div>
+                <div className="w-1/3 h-100"><img src={FollowUsMainImg} alt="Follow Us Main Image" /></div>
+                <div className="w-1/3 h-100 flex flex-row flex-wrap  gap-5">
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg5} alt="Follow Sub Image" /></div>
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg6} alt="Follow Sub Image" /></div>
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg7} alt="Follow Sub Image" /></div>
+                    <div className="w-[47.5%] aspect-square"><img className="w-full aspect-square shadow-xs" src={FollowImg8} alt="Follow Sub Image" /></div>
+                </div>
             </div>
         </div>
     </div>)
