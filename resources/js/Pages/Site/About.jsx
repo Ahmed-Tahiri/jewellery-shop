8e03e3aab2b5ee8c8028e07600883df99c9ece7e
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-15 17:37:12 +0500

    extra lines deleted

--- resources/js/Pages/Site/About.jsx
+++ resources/js/Pages/Site/About.jsx
@@ -1,8 +1,4 @@
-
 import { AboutLanding } from "../../Components/AboutLanding";
-import { Navbar } from "../../Components/MyAccount/Navbar";
-import { NewArrivals } from "../../Components/NewArrivals";
-import { SaleSlider } from "../../Components/SaleSlider";
 import MainLayout from "../../Layouts/MainLayout";
 
 export default function About() {
