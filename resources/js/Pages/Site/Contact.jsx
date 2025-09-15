20199faa99b1b3687643c4855e1b2e50da974795
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-15 21:26:31 +0500

    contact cards added

--- resources/js/Pages/Site/Contact.jsx
+++ resources/js/Pages/Site/Contact.jsx
@@ -2,9 +2,41 @@ import { IoSparklesSharp } from "react-icons/io5";
 import MainLayout from "../../Layouts/MainLayout";
 import ContactImg from './../../../images/Contactimg.jpg';
 import { ContactForm } from "../../Components/ContactForm";
+import { PiMapPinLight } from "react-icons/pi";
+import { TbPhoneCall } from "react-icons/tb";
+import { HiOutlineEnvelope } from "react-icons/hi2";
 
-
+let ContactDataCard = ({ label, data, icon }) => {
+    return <div className="flex-1 w-full max-w-100 flex items-center justify-center flex-col gap-y-5 bg-powder-gray p-10 shadow-sm min-h-60">
+        <div className="relative inline-flex items-center justify-center">
+            {icon}
+            <span className="w-4 h-4 absolute top-5 right-4 bg-mustard rounded-full shadow-xs" />
+        </div>
+        <div className="flex flex-col items-center gap-y-2">
+            <p className="font-poppins text-xl font-medium text-semi-black text-center">{label}</p>
+            <p className="font-poppins text-sm font-medium text-dark-gray text-center">{data}</p>
+        </div>
+    </div>
+}
 export default function Contact() {
+
+    let contactDetails = [
+        {
+            label: 'Address',
+            data: 'Warehouse #5, Industrial Zone, I-9/3, Islamabad, Pakistan',
+            icon: <PiMapPinLight className="text-6xl text-zinc relative z-10" />
+        },
+        {
+            label: 'Phone',
+            data: '+0123-456-789',
+            icon: <TbPhoneCall className="text-6xl text-zinc relative z-10" />
+        },
+        {
+            label: 'Email',
+            data: 'example@gmail.com',
+            icon: <HiOutlineEnvelope className="text-6xl text-zinc relative z-10" />
+        },
+    ];
     return <>
         <div className="flex relative bg-white justify-center items-center py-20 px-5 md:px-10 lg:px-15 xl:px-20 overflow-hidden">
             <div className="w-full flex flex-row items-start justify-between  max-w-7xl gap-x-20 ">
@@ -12,7 +44,7 @@ export default function Contact() {
                     <div className="w-full flex flex-col gap-y-3">
                         <p className="font-poppins text-3xl text-semi-black font-medium">Get in Touch</p>
                         <p className="font-poppins text-base text-semi-black">Your email address will not be published. Required fields are marked* </p>
-                        <div className="w-full"><ContactForm /></div>
+                        <div className="w-full mt-5"><ContactForm /></div>
                     </div>
                 </div>
                 <div className="w-4/10 relative h-150">
@@ -22,12 +54,14 @@ export default function Contact() {
                         <div className="w-full h-full border-2 border-white bg-transparent"></div>
                     </div>
                 </div>
-
-
+            </div>
+        </div>
+        <div className="flex bg-white justify-center items-center py-20 px-5 md:px-10 lg:px-15 xl:px-20 overflow-hidden">
+            <div className="w-full flex flex-row items-start justify-center  max-w-7xl gap-x-10">
+                {contactDetails.map((contact, idx) => <ContactDataCard key={`contactDataCard${idx + 1}`} label={contact.label} icon={contact.icon} data={contact.data} />)}
             </div>
         </div>
     </>
-
 }
 
 Contact.layout = page => (
