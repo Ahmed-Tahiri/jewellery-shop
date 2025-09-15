919909cca0809f780bdf2b3d67b5352ee6736486
Ahmed-Tahiri <ahmedtahiri.webdev@gmail.com>
2025-09-15 19:04:05 +0500

    bug fixed

--- resources/js/Components/OurTeam.jsx
+++ resources/js/Components/OurTeam.jsx
@@ -1,11 +1,14 @@
 import { SectionMainHeading } from "../Shared/SectionMainHeading"
 import { SectionSubHeading } from "../Shared/SectionSubHeading"
+import CEOImage from './../../images/CEOImg.jpg';
+import Designer1Img from './../../images/DesignerImg.jpg';
+import Designer2Img from './../../images/Designer2Img.jpg';
 
 
-export let TeamMemberCard = ({ profile, name, designation }) => {
+export let TeamMemberCard = ({ imgSrc, name, designation }) => {
     return (<div className="flex flex-col gap-y-4">
         <div className="w-60 aspect-square bg-powder-gray shadow-xs">
-            <img src={designation} className="w-full aspect-square" alt="Team Member's Image" />
+            <img src={imgSrc} className="w-full aspect-square object-cover" alt="Team Member's Image" />
         </div>
         <div className="w-60 flex flex-col gap-y-1 items-center">
             <p className="font-poppins text-semi-black font-medium text-xl">{name}</p>
@@ -22,9 +25,9 @@ export let OurTeam = () => {
                 <SectionMainHeading heading={'MEET OUR TEAM'} />
             </div>
             <div className='w-full gap-8 flex flex-row items-center justify-center'>
-                <TeamMemberCard name={'Jenny Alexander'} designation={'CEO, Jewellery Store'} />
-                <TeamMemberCard name={'Leslie Alexander'} designation={'Designer'} />
-                <TeamMemberCard name={'Cody Fisher'} designation={'Designer'} />
+                <TeamMemberCard name={'Jenny Alexander'} designation={'CEO, Jewellery Store'} imgSrc={CEOImage} />
+                <TeamMemberCard name={'Leslie Alexander'} designation={'Designer'} imgSrc={Designer1Img} />
+                <TeamMemberCard name={'Cody Fisher'} designation={'Designer'} imgSrc={Designer2Img} />
             </div>
         </div>
     </section>
